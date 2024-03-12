bd9f3669aa55da22bf521a141dc32fde9f643d30
Ales17 <alexvillar10@gmail.com>
2024-03-12 15:14:21 +0100

    fix

--- app/src/main/java/com/projecte3/provesprojecte/EmployeeDetailsActivity.kt
+++ app/src/main/java/com/projecte3/provesprojecte/EmployeeDetailsActivity.kt
@@ -1,7 +1,6 @@
 package com.projecte3.provesprojecte
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -9,8 +8,9 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.google.firebase.database.FirebaseDatabase
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.google.firebase.database.FirebaseDatabase
 
 
 class EmployeeDetailsActivity : AppCompatActivity() {
@@ -159,7 +159,7 @@ class EmployeeDetailsActivity : AppCompatActivity() {
                 etEmpComun.text.toString()
             )
 
-            Toast.makeText(applicationContext, "Employee Data Updated", Toast.LENGTH_LONG).show()
+            Toast.makeText(applicationContext, "Seta Data Updated", Toast.LENGTH_LONG).show()
 
             //we are setting updated data to our textviews
             tvEmpNombre_comun.text = etEmpNombre_comun.text.toString()
